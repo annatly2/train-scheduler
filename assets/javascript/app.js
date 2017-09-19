b28622398e698a2d30bcef5d12c480bf4629ca38
Anna Ly <annatly2@gmail.com>
2017-09-18 21:34:55 -0700

    sept 18

---
+++ assets/javascript/app.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+console.log("hi");
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDmjG0ZUQcZZpeJ8EthXI67ijcvYCzOYw8",
+    authDomain: "train-scheduler-d4f27.firebaseapp.com",
+    databaseURL: "https://train-scheduler-d4f27.firebaseio.com",
+    projectId: "train-scheduler-d4f27",
+    storageBucket: "train-scheduler-d4f27.appspot.com",
+    messagingSenderId: "1026606938343"
+  };
+  firebase.initializeApp(config);
+
+
+
+
+
+
+
+
+
+});
