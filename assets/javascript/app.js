0b3c6fa7d831519f34aa62d07d58ac6ca83648cf
Anna Ly <annatly2@gmail.com>
2017-09-18 22:42:27 -0700

    sept 18

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
 
-console.log("hi");
 
   // Initialize Firebase
   var config = {
@@ -13,10 +12,42 @@ console.log("hi");
   };
   firebase.initializeApp(config);
 
+var database = firebase.database();
+
+//Button for adding new train
+$("#add-train-btn").on("click", function(event){
+  event.preventDefault();
+
+//Grabs user input
+var trainName = $("#train-name-input").val().trim();
+var destination = $("#destination-input").val().trim();
+var trainTime = moment($("#train-time-input").val().trim(),"HH:mm").format("HH:mm");
+var frequency = $("#frequency-input").val().trim();
+
+//Sets input in new temporary object
+var newTrain = {
+  name: trainName,
+  dest: destination,
+  time: trainTime,
+  freq: frequency
+};
+
+//Uploads train data to the database
+database.ref().push(newTrain);
+
+console.log(newTrain.name);
+console.log(newTrain.dest);
+console.log(newTrain.time);
+console.log(newTrain.freq);
+
+//Clears text-boxes
+$("#train-name-input").val("");
+$("#destination-input").val("");
+$("#train-time-input").val("");
+$("#frequency-input").val("");
 
 
-
-
+});
 
 
 
